69cfed874074d37d777f1263c4dbde5e9b093ac1
tr1503 <hanlinye6@gmail.com>
2018-09-11 22:26:46 -0400

    House Robber II Problem

---
+++ Dynamic Programming/houseRobber_II.py
@@ -0,0 +1,31 @@
+'''Use Dynamic Programming to get the max sums between 0 to last - 1 and between 1 to last.
+The result is max of these two.'''
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        sums1 = [0 for _ in range(len(nums))]
+        sums2 = [0 for _ in range(len(nums))]
+        for i in range(len(sums1) - 1):
+            if i == 0:
+                sums1[i] = nums[i]
+            elif i == 1:
+                sums1[i] = max(nums[i],nums[i-1])
+            else:
+                sums1[i] = max(sums1[i-2] + nums[i], sums1[i-1])
+        ans1 = sums1[len(sums1) - 2]
+        for i in range(1,len(sums2)):
+            if i == 1:
+                sums2[i] = nums[i]
+            elif i == 2:
+                sums2[i] = max(nums[i],nums[i-1])
+            else:
+                sums2[i] = max(sums2[i-2] + nums[i], sums2[i-1])
+        ans2 = sums2[len(sums2) - 1]
+        return max(ans1,ans2)
