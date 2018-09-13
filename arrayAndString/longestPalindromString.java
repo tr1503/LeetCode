9591bed83f52c381370736a6f37e0d93390e2264
tr1503 <hanlinye6@gmail.com>
2018-09-12 22:08:35 -0400

    Longest Palindromic Substring Problem

---
+++ arrayAndString/longestPalindromString.java
@@ -0,0 +1,30 @@
+/*
+Use expand around center to get the each number's palindrome.
+Time is O(n^2) and space is O(1). This question has different ways including dp and manacer's algorithm.
+The website here explain them: http://windliang.cc/2018/08/05/leetCode-5-Longest-Palindromic-Substring/
+*/
+class Solution {
+    public String longestPalindrome(String s) {
+    if (s == null || s.length() < 1) return "";
+    int start = 0, end = 0;
+    for (int i = 0; i < s.length(); i++) {
+        int len1 = expandAroundCenter(s, i, i);
+        int len2 = expandAroundCenter(s, i, i + 1);
+        int len = Math.max(len1, len2);
+        if (len > end - start) {
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
+        }
+    }
+    return s.substring(start, end + 1);
+}
+
+private int expandAroundCenter(String s, int left, int right) {
+    int L = left, R = right;
+    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+        L--;
+        R++;
+    }
+    return R - L - 1;
+    }
+}
