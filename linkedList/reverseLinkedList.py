6b202d56309ded29ca20f4263e3b0dd8f353eacd
tr1503 <hanlinye6@gmail.com>
2018-09-22 11:06:50 -0400

    Reverse Linked List Problem

---
+++ linkedList/reverseLinkedList.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        #iterative way:
+        '''prev = None
+        tail = head
+        while tail is not None:
+            temp = tail.next
+            tail.next = prev
+            prev = tail
+            tail = temp
+        return prev'''
+        #recursive way:
+        if head is None or head.next is None:
+            return head
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return p
