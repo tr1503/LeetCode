9218ca65d122f076b7907943365766c2aaf728f9
tr1503 <hanlinye6@gmail.com>
2018-09-26 20:15:25 -0400

    Longest Common Prefix Problem

---
+++ arrayAndString/longtestCommonPrefix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        for i, letters in enumerate(zip(*strs)):
+            if len(set(letters)) > 1:
+                return strs[0][:i]
+        else:
+            return min(strs)
