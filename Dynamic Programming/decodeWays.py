d2854ffb61268346983f013ee0a1cf65d7f7ae26
tr1503 <hanlinye6@gmail.com>
2018-09-29 21:09:40 -0400

    Decode Ways Problem

---
+++ Dynamic Programming/decodeWays.py
@@ -0,0 +1,23 @@
+'''Use a similar way as Fib. Check the last value in string. 
+If the last value is 0, the decode ways at this value should be 0.
+Otherwise the dp value should add dp[i-1]. 
+If the position is not at beginning and the combination of last two values are smaller than
+26 and larger than 10, add dp[i-2].'''
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s or s[0] == "0":
+            return 0
+        dp = [0 for _ in range(len(s) + 1)]
+        dp[0] = 1
+        for i in range(1,len(dp)):
+            if s[i-1] == '0':
+                dp[i] = 0
+            else:
+                dp[i] = dp[i-1]
+            if i > 1 and (s[i - 2] == '1' or (s[i-2] == '2' and s[i-1] <= '6')):
+                dp[i] += dp[i-2]
+        return dp[len(dp) - 1]
