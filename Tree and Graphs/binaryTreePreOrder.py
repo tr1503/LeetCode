daa3efa02e26dc36d67204c8967078cbaf493cc5
tr1503 <hanlinye6@gmail.com>
2018-10-09 16:20:46 -0400

    Binary Tree Preorder

---
+++ Tree and Graphs/binaryTreePreOrder.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        res = []
+        stack = [root]
+        while len(stack) != 0:
+            root = stack.pop()
+            res.append(root.val)
+            if root.right:
+                stack.append(root.right)
+            if root.left:
+                stack.append(root.left)
+        return res
