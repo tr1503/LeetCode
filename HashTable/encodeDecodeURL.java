d8e02ba4cb21fea2ecc412e97d602600a227e67a
tr1503 <hanlinye6@gmail.com>
2018-10-12 20:22:50 -0400

    Encode and Decode TinyURL

---
+++ HashTable/encodeDecodeURL.java
@@ -0,0 +1,18 @@
+public class Codec {
+    Map<String,String> map = new HashMap<>();
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        String s = Integer.toHexString(longUrl.hashCode());
+        map.put(s,longUrl);
+        return s;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(shortUrl);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
