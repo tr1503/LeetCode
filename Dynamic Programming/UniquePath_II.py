9cf4873d3eed313754cbdd263de8de1e3b2a02bc
tr1503 <hanlinye6@gmail.com>
2018-10-14 03:16:00 -0400

    Unique Path II

---
+++ Dynamic Programming/UniquePath_II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        n = len(obstacleGrid)
+        m = len(obstacleGrid[0])
+        dp = [0 for _ in range(m)]
+        dp[0] = 1
+        for i in range(n):
+            for j in range(m):
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j-1]
+        return dp[m-1]
