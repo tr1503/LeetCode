bbd0adbdb7d3452d1eb144f894fe75d8a6bc745a
tr1503 <hanlinye6@gmail.com>
2018-10-15 14:44:20 -0400

    Update generalizeAbbreviations.java

--- Backtracking/generalizeAbbreviations.java
+++ Backtracking/generalizeAbbreviations.java
@@ -11,14 +11,16 @@ class Solution {
             return;
         }
         
+        //New start position
         int i = 0;
-        
+        //Except first position, others need to start at the next position
         if (start > 0) {
             i = start + 1;
         }
         
         for (; i < s.length(); i++) {
             String prefix = curr + s.substring(start,i);
+            //From ith character, change to numbers from 1 to s.length - i
             for (int j = 1; j <= s.length() - i; j++) {
                 dfs(res, prefix + j, i + j, s);
             }
