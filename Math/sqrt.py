77dee4f8db05a6dcbbc2ba0e37cecd6f0f38b3a3
tr1503 <hanlinye6@gmail.com>
2018-10-16 22:13:18 -0400

    Update sqrt.py

--- Math/sqrt.py
+++ Math/sqrt.py
@@ -6,9 +6,25 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+        #Newton method:
         if x == 0 or x == 1:
             return x
         guess = x / 2
-        for i in range(100):
+        for i in range(19):
             guess = (guess + x / guess) / 2.0
         return int(guess)
+        #Binary Search:
+        '''lo = 0
+        hi = x
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                hi = mid - 1
+            else:
+                if (mid + 1) * (mid + 1) > x:
+                    return mid
+                else:
+                    lo = mid + 1
+        return lo'''
