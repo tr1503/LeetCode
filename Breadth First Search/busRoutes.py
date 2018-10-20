c45413b4a081702004a37c189f9f7c24cce21ac6
tr1503 <hanlinye6@gmail.com>
2018-10-20 02:34:52 -0400

    Bus Routes

---
+++ Breadth First Search/busRoutes.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numBusesToDestination(self, routes, S, T):
+        """
+        :type routes: List[List[int]]
+        :type S: int
+        :type T: int
+        :rtype: int
+        """
+        if S == T:
+            return 0
+        m = {}
+        #Use hashmap to build a bus->stops, like bus1 has some stops, bus2 has some stops
+        for i in range(len(routes)):
+            for stop in routes[i]:
+                if stop in m:
+                    m[stop].append(i)
+                else:
+                    m[stop] = []
+                    m[stop].append(i)
+        visited = [0 for _ in range(len(routes))]
+        queue = []
+        queue.append(S)
+        buses = 0
+        while len(queue) != 0:
+            size = len(queue)
+            buses += 1
+            for i in range(size):
+                curr = queue.pop(0)
+                for bus in m[curr]:
+                    if visited[bus] == 1:
+                        continue
+                    visited[bus] = 1
+                    for stop in routes[bus]:
+                        if stop == T:
+                            return buses
+                        queue.append(stop)
+        return -1
