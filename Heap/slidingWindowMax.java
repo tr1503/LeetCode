31ef31a6e48714db490821b582e5116cdf787790
tr1503 <hanlinye6@gmail.com>
2018-10-23 23:58:07 -0400

    Sliding Window Maximum

---
+++ Heap/slidingWindowMax.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || k <= 0)
+            return new int[0];
+        int n = nums.length;
+        int[] r = new int[n - k + 1];
+        int j = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < nums.length; i++) {
+            // remove numbers out of range k
+            while (!queue.isEmpty() && queue.peek() < i - k + 1) {
+                queue.poll();
+            }
+            // remove smaller numbers in k range as they are useless
+            while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
+                queue.pollLast();
+            }
+            queue.offer(i);
+            if (i >= k - 1) {
+                r[j++] = nums[queue.peek()];
+            }
+        }
+        return r;
+    }
+}
