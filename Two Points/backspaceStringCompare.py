ac1aae6257eac6038ab08501cc095b4197019bcf
tr1503 <hanlinye6@gmail.com>
2018-10-25 18:10:59 -0400

    Backspace String Compare

--- Two Points/backspaceStringCompare.py
+++ Two Points/backspaceStringCompare.py
@@ -5,6 +5,20 @@ class Solution(object):
         :type T: str
         :rtype: bool
         """
+        #Stack way: Time O(n), space O(n)
+        stack1 = []
+        stack2 = []
+        for c in S:
+            if len(stack1) != 0 and c == '#':
+                stack1.pop()
+            if c != '#':
+                stack1.append(c)
+        for c in T:
+            if len(stack2) != 0 and c == '#':
+                stack2.pop()
+            if c != '#':
+                stack2.append(c)
+        return str(stack1) == str(stack2)
         #Two pointers: time O(n) space O(1)
         si, ti = len(S) - 1, len(T) - 1
         count_s = count_t = 0
@@ -42,3 +56,15 @@ class Solution(object):
             si -= 1
             ti -= 1
         return True
+        
+        # Other way for two points with O(n) time and O(1) space
+        def F(S):
+            skip = 0
+            for x in reversed(S):
+                if x == '#':
+                    skip += 1
+                elif skip:
+                    skip -= 1
+                else:
+                    yield x
+        return all(x == y for x, y in itertools.izip_longest(F(S),F(T)))
