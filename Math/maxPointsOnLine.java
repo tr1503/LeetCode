f14a789e82dcebbbcca24e2dd3ca8872c845efa2
tr1503 <hanlinye6@gmail.com>
2018-10-25 00:08:46 -0400

    Max Points On Line

---
+++ Math/maxPointsOnLine.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+class Solution {
+    public int maxPoints(Point[] points) {
+        int res = 0;
+        for (int i = 0; i < points.length; i++) {
+            Map<Map<Integer,Integer>, Integer> m = new HashMap<>();
+            int duplicate = 1;
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].x == points[j].x && points[i].y == points[j].y) {
+                    duplicate++;
+                    continue;
+                }
+                int dx = points[j].x - points[i].x;
+                int dy = points[j].y - points[i].y;
+                int d = gcd(dx,dy);
+                Map<Integer,Integer> t = new HashMap<>();
+                t.put(dx / d, dy / d);
+                m.put(t, m.getOrDefault(t,0) + 1);
+            }
+            res = Math.max(res, duplicate);
+            for (Map.Entry<Map<Integer,Integer>, Integer> e : m.entrySet()) {
+                res = Math.max(res, e.getValue() + duplicate);
+            }
+        }
+        return res;
+    }
+    
+    private int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
+    }
+}
