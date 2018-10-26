128c6c88747466e124e8a013ffb783829aea6076
tr1503 <hanlinye6@gmail.com>
2018-10-26 01:19:50 -0400

    Index Difference

---
+++ Stack/indexDiff.py
@@ -0,0 +1,20 @@
+def indexDiff(nums):
+	if len(nums) == 0:
+		return []
+	res = [-1 for _ in range(len(nums))]
+	stack = [] # add [number, index]
+
+	for i, num in enumerate(nums):
+		while len(stack) != 0 and num > stack[-1][0]:
+			smallIndex = stack[-1][1]
+			res[smallIndex] = i - smallIndex
+			stack.pop()
+		stack.append([num,i])
+	return res
+
+def main():
+	nums = [10, 8, 6, 8, 8, 11, 9]
+	print(indexDiff(nums))
+
+if __name__ == '__main__':
+	main()
