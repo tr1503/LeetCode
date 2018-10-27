0a5e50add5a2ae524282659b8668a3ac972f932a
tr1503 <hanlinye6@gmail.com>
2018-10-27 00:48:58 -0400

    LRU Cache

--- Design/LRU.py
+++ Design/LRU.py
@@ -1,23 +1,35 @@
-class LRUCache(object):
+# Use dictionary and doubly linked list to do this quesiton for O(1) time
+class Node:
+    def __init__(self, k, v):
+        self.key = k
+        self.val = v
+        self.prev = None
+        self.next = None
+        
+class LRUCache:
 
     def __init__(self, capacity):
         """
         :type capacity: int
         """
         self.capacity = capacity
-        self.cache = collections.OrderedDict()
+        self.dic = dict()
+        self.head = Node(0,0)
+        self.tail = Node(0,0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
-        if key not in self.cache:
-            return -1
-        value = self.cache.pop(key)
-        self.cache[key] = value
-        return value
-        
+        if key in self.dic:
+            n = self.dic[key]
+            self._remove(n)
+            self._add(n)
+            return n.val
+        return -1
 
     def put(self, key, value):
         """
@@ -25,13 +37,28 @@ class LRUCache(object):
         :type value: int
         :rtype: void
         """
-        if key in self.cache:
-            self.cache.pop(key)
-        if len(self.cache) == self.capacity:
-            self.cache.popitem(last=False)
+        if key in self.dic:
+            self._remove(self.dic[key])
+        n = Node(key,value)
+        self._add(n)
+        self.dic[key] = n
+        if len(self.dic) > self.capacity:
+            n = self.head.next
+            self._remove(n)
+            del self.dic[n.key]
         
-        self.cache[key] = value
-
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+    
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.prev = p
+        node.next = self.tail
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
