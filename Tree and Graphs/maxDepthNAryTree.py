9cd7d0f858b2bcfd9e634a75663379a1f9771f4a
tr1503 <hanlinye6@gmail.com>
2018-10-27 20:29:16 -0400

    Maximum Depth of N-ary Tree

---
+++ Tree and Graphs/maxDepthNAryTree.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        elif root.children == []:
+            return 1
+        else:
+            height = [self.maxDepth(c) for c in root.children]
+            return max(height) + 1
