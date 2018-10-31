0b31e0a569629cc11c6c1dc2142ccfb2b49314b4
tr1503 <hanlinye6@gmail.com>
2018-10-31 00:48:12 -0400

    Unique Path

--- Dynamic Programming/uniquePath.py
+++ Dynamic Programming/uniquePath.py
@@ -6,6 +6,19 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # Use 2D dp array, space is O(m * n)
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+        for i in range(0,n):
+            for j in range(0,m):
+                if i == 0 and j == 0:
+                    dp[i][j] = 0
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[n-1][m-1]
+    
+        # Use 1D dp array, space is O(n)
         dp = [0 for _ in range(n)]
         dp[0] = 1
         for i in range(m):
