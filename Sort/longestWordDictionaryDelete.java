4b4fc99df1950a063ed25ca7f4a9ab70f9e96547
tr1503 <hanlinye6@gmail.com>
2018-10-30 21:39:06 -0400

    Longest Word in Dictionary Through Deleting

--- Sort/longestWordDictionaryDelete.java
+++ Sort/longestWordDictionaryDelete.java
@@ -14,5 +14,20 @@ class Solution {
             }
         }
         return "";
+        
+        // Without using sort, time is O(nk), k is the average length of words in dictionary
+        String res = "";
+        for (String word : d) {
+            int i = 0;
+            for (char c : s.toCharArray()) {
+                if (i < word.length() && c == word.charAt(i))
+                    i++;
+            }
+            if (i == word.length() && word.length() >= res.length()) {
+                    if (word.length() > res.length() || word.compareTo(res) < 0)
+                        res = word;
+            }
+        }
+        return res;
     }
 }
