15d6b89d229af410f078b20c91a2a7ef5af9481f
tr1503 <hanlinye6@gmail.com>
2018-12-14 23:20:49 -0500

    Reverse String

--- arrayAndString/reverseString.java
+++ arrayAndString/reverseString.java
@@ -1,12 +1,19 @@
 class Solution {
     public String reverseString(String s) {
-        if (s == null) return null;
-        char[] reverse = s.toCharArray();
-        for (int i = 0, j = s.length()-1; j > i; i++, j--) {
-            char temp = reverse[j];
-            reverse[j] = reverse[i];
-            reverse[i] = temp;
-        }
-        return new String(reverse);
+        // if (s == null)
+        //     return null;
+        // char[] reverse = s.toCharArray();
+        // for (int i = 0, j = reverse.length - 1; j > i; i++, j--) {
+        //     char temp = reverse[i];
+        //     reverse[i] = reverse[j];
+        //     reverse[j] = temp;
+        // }
+        // return new String(reverse);
+        int length = s.length();
+        if (length <= 1)
+            return s;
+        String left = s.substring(0, length / 2);
+        String right = s.substring(length / 2, length);
+        return reverseString(right) + reverseString(left);
     }
 }
