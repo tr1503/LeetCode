0687a2e9cb04ceeb6438fbf2c1dad50184b47d04
tr1503 <hanlinye6@gmail.com>
2019-09-08 02:31:14 -0400

    Subtree of Another Tree

---
+++ Depth First Search/isSubtree.py
@@ -0,0 +1,28 @@
+# Use dfs to iterate two trees at same time and compare their nodes
+# time is O(n * m), space is O(n * m)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        def checkTree(root1, root2):
+            if not root1 and not root2:
+                return True
+            elif (root1 and not root2) or (root2 and not root1):
+                return False
+            if root1.val != root2.val:
+                return False
+            return checkTree(root1.left, root2.left) and checkTree(root1.right, root2.right)
+        
+        def dfs(s, t):
+            if not s:
+                return False
+            if s.val == t.val and checkTree(s, t):
+                return True
+            return dfs(s.left, t) or dfs(s.right, t)
+        
+        return dfs(s, t)
