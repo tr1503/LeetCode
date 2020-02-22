57f041c279f340da991bd597f252e28d5d852f00
tr1503 <hanlinye6@gmail.com>
2020-02-21 22:52:44 -0500

    Find N Unique Integers Sum up to Zero

---
+++ arrayAndString/sumZero.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = i * 2 - n + 1;
+        }
+        return res;
+    }
+}
