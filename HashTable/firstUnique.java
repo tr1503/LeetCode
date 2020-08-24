64a6f327ae9960accb5bf4830155967bb07c5355
tr1503 <hanlinye55@gmail.com>
2020-08-24 00:31:31 -0400

    First Unique Number

---
+++ HashTable/firstUnique.java
@@ -0,0 +1,29 @@
+class FirstUnique {
+    
+    private Queue<Integer> queue = new ArrayDeque<>();
+    private Map<Integer, Boolean> map = new HashMap<>();
+    
+    public FirstUnique(int[] nums) {
+        for (int num : nums) {
+            this.add(num);
+        }    
+    }
+    
+    public int showFirstUnique() {
+        while (!queue.isEmpty() && !map.get(queue.peek())) {
+            queue.remove();
+        }
+        if (!queue.isEmpty()) 
+            return queue.peek();
+        return -1;
+    }
+    
+    public void add(int value) {
+        if (!map.containsKey(value)) {
+            map.put(value, true);
+            queue.add(value);
+        }
+        else 
+            map.put(value, false);
+    }
+}
